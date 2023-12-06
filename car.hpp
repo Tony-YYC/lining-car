51ce5e8abf2c2eebcf1cbc8a5d77702655d9a7ac
YYC <yuyicheng0926@outlook.com>
2023-12-06 13:58:23 +0800

    wrap the car module

--- car.hpp
+++ car.hpp
@@ -5,7 +5,7 @@ class Sensor
     const int sensor[5] = {A0, A1, A2, A3, A4};
     const int threshold = 250;
     int sensorReading[5];
-    int lineNum = -1;
+    int lineNum = 0;
     int count = 0;
     void read()
     {
@@ -14,7 +14,8 @@ class Sensor
         for (int i = 0; i < 5; i++)
         {
             sensorReading[i] = analogRead(sensor[i]);
-            if(sensorReading[i] < threshold);
+            if (sensorReading[i] < threshold)
+                ;
             {
                 lineNum = i;
                 ++count;
@@ -22,17 +23,19 @@ class Sensor
             Serial.print(sensorReading[i]);
             Serial.print('-');
         }
+        Serial.println();
     }
 
   public:
     int getStatus()
     {
         read();
-        if(count == 1)return lineNum+1;
-        else return 0;
+        if (count == 1)
+            return lineNum + 1;
+        else
+            return 0;
     }
-}
-;
+};
 class Motor
 {
   private:
@@ -51,8 +54,8 @@ class Motor
     const int turn_speed = 60;
     const int sharp_turn_speed = 30;
 
-    const int turn_angle[2] = {15,15};
-    const int sharp_turn_angle[2] = {30,30};
+    const int turn_angle[2] = {15, 15};
+    const int sharp_turn_angle[2] = {30, 30};
 
     Servo servo;
 
@@ -83,44 +86,79 @@ class Motor
 
     void run()
     {
-        A_Motor(HIGH,straight_speed);
-        B_Motor(HIGH,straight_speed);
+        A_Motor(HIGH, straight_speed);
+        B_Motor(HIGH, straight_speed);
         servo.write(offset);
     }
 
     void left()
     {
-        A_Motor(HIGH,turn_speed);
-        B_Motor(HIGH,turn_speed);
-        servo.write(offset+turn_angle[0]);
+        A_Motor(HIGH, turn_speed);
+        B_Motor(HIGH, turn_speed);
+        servo.write(offset + turn_angle[0]);
     }
 
     void right()
     {
-        A_Motor(HIGH,turn_speed);
-        B_Motor(HIGH,turn_speed);
-        servo.write(offset-turn_angle[1]);
+        A_Motor(HIGH, turn_speed);
+        B_Motor(HIGH, turn_speed);
+        servo.write(offset - turn_angle[1]);
     }
 
     void sharp_left()
     {
-        A_Motor(HIGH,sharp_turn_speed);
-        B_Motor(HIGH,sharp_turn_speed);
-        servo.write(offset+sharp_turn_angle[0]);
+        A_Motor(HIGH, sharp_turn_speed);
+        B_Motor(HIGH, sharp_turn_speed);
+        servo.write(offset + sharp_turn_angle[0]);
     }
 
     void sharp_right()
     {
-        A_Motor(HIGH,sharp_turn_speed);
-        B_Motor(HIGH,sharp_turn_speed);
-        servo.write(offset-sharp_turn_angle[1]);
+        A_Motor(HIGH, sharp_turn_speed);
+        B_Motor(HIGH, sharp_turn_speed);
+        servo.write(offset - sharp_turn_angle[1]);
     }
 
     void stop()
     {
-        A_Motor(HIGH,0);
-        B_Motor(HIGH,0);
+        A_Motor(HIGH, 0);
+        B_Motor(HIGH, 0);
         servo.write(offset);
+    }
+};
+
+class Car : private Sensor, private Motor
+{
+  public:
+    Car()
+    {
+        Motor();
+    }
+
+    void run_with_tracing()
+    {
+        int status = Sensor::getStatus();
+        Serial.println(status);
+        switch (Sensor::getStatus())
+        {
+        case 0:
+            Motor::stop();
+        case 1:
+            Motor::sharp_left();
+        case 2:
+            Motor::left();
+        case 3:
+            Motor::run();
+        case 4:
+            Motor::right();
+        case 5:
+            Motor::sharp_right();
+        }
+    }
+
+    void run_without_tracing()
+    {
+        Motor::run();
     }
 }
 ;
